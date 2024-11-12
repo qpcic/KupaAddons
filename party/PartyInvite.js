3434dde754631eef33e23b3982edcf251b1e54fc
Jan Kupčič <kupcic.jan@gmail.com>
2024-11-12 21:33:06 +0100

    Party invite cooldown

--- party/PartyInvite.js
+++ party/PartyInvite.js
@@ -4,13 +4,18 @@ import {
 } from "../utils/Constants";
 import {data} from "../utils/Data";
 
-const list = data.permlist;
+function stopFor(ms) {
+    const stoptime = Date.now() + ms;
+    while (Date.now() < stoptime) continue;
+}
 
+const list = data.permlist;
 export function partyInv(args) {
     ChatLib.chat(`${LOGO} ${DARK_PURPLE}Inviting players...`);
     if (list.length > 0) {
         for (let i = 0; i < list.length; i++) {
             ChatLib.command(`p invite ${list[i].toString()}`);
+            stopFor(550);
         }
     }
 }
